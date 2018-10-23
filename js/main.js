ab85d458806d528ef92d8c385083ee37a56dff17
Rafi.M <rafi.muc@outlook.com>
2018-10-23 14:52:13 +0300

    14:52

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function init(){
+    renderImgs();
+}
+
+function renderImgs() {
+    var elHtmlStr = '';
+    gImgs.forEach((element) => {
+        elHtmlStr += `<div class="imgDiv flex"><img src="${element.imgUrl}" alt=""></img></div>`
+    });
+    document.querySelector('.container').innerHTML = elHtmlStr;
+}
