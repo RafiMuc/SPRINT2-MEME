3d3b61eeb96ea425700e58acf00ce0812579ba6b
sggv950 <38014323+sggv950@users.noreply.github.com>
2018-10-23 15:23:52 +0300

    canvas

--- js/main.js
+++ js/main.js
@@ -7,13 +7,17 @@ function init(){
 function renderImgs() {
     var elHtmlStr = '';
     gImgs.forEach((element) => {
-        elHtmlStr += `<div class="imgDiv flex"><img src="${element.imgUrl}" onclick="onSetImgOnCanvas(this)"></div>`
+        elHtmlStr += `<div class="imgDiv flex"><img src="${element.imgUrl}" onclick="onSetImgOnCanvas(this)" /></div>`
     });
-    document.querySelector('.container').innerHTML = elHtmlStr;
+    document.querySelector('.gallery-container').innerHTML = elHtmlStr;
 }
 
 function onSetImgOnCanvas(elImg){
+    var elGallery = document.querySelector('.gallery-container');
+    var elCanvas = document.querySelector('.meme-canvas');
     clearCanvas();
+    elGallery.style.display = 'none';
+    elCanvas.display = 'block';
     setImgOnCanvas(elImg);
 }
 
